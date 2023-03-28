330c95f56e71ab5e88b22bd2d0db33fae8b74c89
cyx-r <rkukoyev@gmail.com>
2023-03-28 14:23:59 +0300

    ft: +WeatherModel class +WeatherClientAPI class

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:weather_forecast_v1/MainPage.dart';
 import 'MyApp.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
-
-
-
---
+++ lib/services/weather_api_client.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:weather_forecast_v1/models/weather_model.dart';
+
+class WeatherApiClient {
+  Future<WeatherModel>? getCurrentWeather(String? location) async {
+    var endpoint = Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?q=$location&appid={5715ef1423a2d2f685746cbc6f9d4eca}");
+    var response = await http.get(endpoint);
+    var body = jsonDecode(response.body);
+    return WeatherModel.fromJson(body);
+  }
+}
