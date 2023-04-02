bd9cdca9a46c4352a9b4bab488d3445a59116032
Roman <rkukoyev@mail.ru>
2023-04-03 01:26:41 +0600

    almost done

--- lib/ui/MainPage.dart
+++ lib/ui/MainPage.dart
@@ -1,13 +1,22 @@
+import 'dart:io';
+
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
 import 'package:weather/weather.dart';
-
+import 'package:weather_forecast_v1/models/current_weather_card.dart';
+import 'package:weather_forecast_v1/services/weather_api_client.dart';
+import 'package:location/location.dart';
 
 class MainPage extends StatefulWidget {
   final String route = "/";
   MainPage({super.key});
 
+  late WeatherFactory wf = WeatherFactory(APIkey);
+  static const String APIkey = "5715ef1423a2d2f685746cbc6f9d4eca";
+  List<Weather> _data = [] ;
+
+  final Location userLocation = Location();
   
 
   @override
@@ -15,59 +24,79 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  late WeatherFactory wf = WeatherFactory(APIkey);
-  static const String APIkey = "5715ef1423a2d2f685746cbc6f9d4eca";
-  List<Weather> _data = [] ;
-
-  @override 
-  void initState(){
-    super.initState();
-    wf = WeatherFactory(APIkey);
+  bool isdarkTheme = true;
+  late LocationData currentLocation = Future.value();
+  Future<Weather> getCurrentWeather(double? lat, double? long) async{
+    try{
+      var weather = await widget.wf.currentWeatherByLocation(lat!, long!);
+      return weather;
+    }
+    catch(NullPointerException){
+      print("Location is null");
+    }
+    throw "ooooooooooo";
   }
 
-  Future<Weather> getcurrentWeather() async{
-    var weather = await wf.currentWeatherByCityName("Omsk");
-    return weather;
-  }
 
   void getfiveDayForecast() async{
-    List<Weather> forecast = await wf.fiveDayForecastByCityName("Omsk");
+    List<Weather> forecast = await widget.wf.fiveDayForecastByCityName("Omsk");
     setState(() {
-      _data = forecast;
+      widget._data = forecast;
     });  
   }
+
+  Future<LocationData> checkLocationPermission() async{
+    var serviceEnabled = await widget.userLocation.serviceEnabled();
+    if(!serviceEnabled){
+      serviceEnabled = await widget.userLocation.requestService();
+    }
+    var permissionGranted = await widget.userLocation.hasPermission();
+    if(permissionGranted != PermissionStatus.granted) {
+      permissionGranted = await widget.userLocation.requestPermission();
+    }
+    if(permissionGranted == PermissionStatus.granted || permissionGranted == PermissionStatus.grantedLimited ){
+      return widget.userLocation.getLocation();
+    }
+    return widget.userLocation.getLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-      appBar: AppBar(
-        title: const Text("Forecast"),
-        actions: [
-          IconButton(
-          icon: Icon(Icons.settings),
-          onPressed: (){
-            Navigator.of(context).pushNamed("/settings");
-          },
+    checkLocationPermission();
+    return Scaffold(
+        backgroundColor: isdarkTheme ? Color.fromARGB(255, 83, 83, 83) : Colors.white,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: isdarkTheme ? Colors.black : Color.fromARGB(255, 128, 193, 247),
+          title: Text("Weather Forecast"),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.lightbulb),
+              onPressed: (){
+                setState(() {
+                  isdarkTheme = !isdarkTheme;
+                });
+              },
+            ),
+          ] 
         ),
-        ]
-      ),
-      body: FutureBuilder(
-        future: getcurrentWeather(),
-        builder: (context, snapshot){
-          switch(snapshot.connectionState){
-            
-            case ConnectionState.none:
-              return CircularProgressIndicator();
-            case ConnectionState.waiting:
-              return CircularProgressIndicator();
-            case ConnectionState.active:
-              return CircularProgressIndicator();
-            case ConnectionState.done:
-              return Center(
-                child:  Text(snapshot.data!.tempFeelsLike.toString()),
-              );
-          }
-        }
-      ),
-    );
+        body: FutureBuilder(
+          future: getCurrentWeather(currentLocation.latitude,currentLocation.longitude),
+          builder:(context, snapshot) {
+            switch(snapshot.connectionState){
+              
+              case ConnectionState.none:
+                return Center(child: CircularProgressIndicator(),);
+              case ConnectionState.waiting:
+                return Center(child: CircularProgressIndicator(),);
+              case ConnectionState.active:
+                return Center(child: CircularProgressIndicator(),);
+
+              case ConnectionState.done:
+                return CurrentWeatherCard(isdarkTheme: isdarkTheme, weather: snapshot.data);
+            }
+          }, 
+        ),
+      );
   }
 }
